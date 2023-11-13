8604b5a2d0659cc948241b69fef2898f796d671a
Bart Duisters <bartduisters@bartduisters.com>
2023-11-13 20:37:12 +0100

    feat: use styled button in main layout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { StyledButtonComponent } from '../../components/styled-button/styled-button.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, StyledButtonComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
