db39047cb01a32ed2ffaa2c92a82fa4aef3be9ce
Bart Duisters <bartduisters@bartduisters.com>
2023-11-14 09:20:05 +0100

    feat: hero-details component

--- src/app/pages/hero-details/hero-details.component.ts
+++ src/app/pages/hero-details/hero-details.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { StyledButtonComponent } from '../../components/styled-button/styled-button.component';
+
+interface Hero {
+  number: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-hero-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StyledButtonComponent],
   templateUrl: './hero-details.component.html',
-  styleUrl: './hero-details.component.css'
+  styleUrl: './hero-details.component.css',
 })
 export class HeroDetailsComponent {
-
+  hero: null | Hero = { number: 15, name: 'Magneta' };
 }
