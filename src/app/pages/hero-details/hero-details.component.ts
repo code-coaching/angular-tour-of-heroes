9f45855d2bb667bd8b89e47be4123b463ec6f28b
Bart Duisters <bartduisters@bartduisters.com>
2023-11-14 19:41:22 +0100

    feat: hero service

--- src/app/pages/hero-details/hero-details.component.ts
+++ src/app/pages/hero-details/hero-details.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from '../../components/models';
 import { StyledButtonComponent } from '../../components/styled-button/styled-button.component';
-import { FormsModule } from '@angular/forms';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -14,26 +15,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class HeroDetailsComponent implements OnInit {
   hero: null | Hero = null;
-  heroes = [
-    { number: 11, name: 'Mr. Nice' },
-    { number: 12, name: 'Narco' },
-    { number: 13, name: 'Bombasto' },
-    { number: 14, name: 'Celeritas' },
-    { number: 15, name: 'Magneta' },
-    { number: 16, name: 'RubberMan' },
-    { number: 17, name: 'Dynama' },
-    { number: 18, name: 'Dr IQ' },
-    { number: 19, name: 'Magma' },
-    { number: 20, name: 'Tornado' },
-  ];
 
-  constructor(private route: ActivatedRoute, public router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    public router: Router,
+    private heroService: HeroService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       const id = +params['id'];
       if (id) {
-        const matchingHero = this.heroes.find((h) => h.number === id);
+        const matchingHero = this.heroService.heroes.find(
+          (h) => h.number === id
+        );
         if (matchingHero) {
           this.hero = matchingHero;
         }
--- src/app/pages/hero-list/hero-list.component.ts
+++ src/app/pages/hero-list/hero-list.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { StyledButtonComponent } from '../../components/styled-button/styled-button.component';
 import { Hero } from '../../components/models';
+import { StyledButtonComponent } from '../../components/styled-button/styled-button.component';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-hero-list',
@@ -12,20 +13,7 @@ import { Hero } from '../../components/models';
   styleUrl: './hero-list.component.css',
 })
 export class HeroListComponent {
-  constructor(public router: Router) {}
-
-  heroes = [
-    { number: 11, name: 'Mr. Nice' },
-    { number: 12, name: 'Narco' },
-    { number: 13, name: 'Bombasto' },
-    { number: 14, name: 'Celeritas' },
-    { number: 15, name: 'Magneta' },
-    { number: 16, name: 'RubberMan' },
-    { number: 17, name: 'Dynama' },
-    { number: 18, name: 'Dr IQ' },
-    { number: 19, name: 'Magma' },
-    { number: 20, name: 'Tornado' },
-  ];
+  constructor(public router: Router, public heroService: HeroService) {}
 
   selectedHero: null | Hero = null;
 
