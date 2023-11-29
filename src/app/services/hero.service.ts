63011bcd8676fd61582e34846d6e7b47460b507f
Bart Duisters <bartduisters@bartduisters.com>
2023-11-29 20:54:20 +0100

    feat: loadHeroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,26 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Hero } from '../components/models';
+import { Hero, HeroBackend } from '../components/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   selectedHero: null | Hero = null;
-
-  heroes = [
-    { number: 11, name: 'Mr. Nice' },
-    { number: 12, name: 'Narco' },
-    { number: 13, name: 'Bombasto' },
-    { number: 14, name: 'Celeritas' },
-    { number: 15, name: 'Magneta' },
-    { number: 16, name: 'RubberMan' },
-    { number: 17, name: 'Dynama' },
-    { number: 18, name: 'Dr IQ' },
-    { number: 19, name: 'Magma' },
-    { number: 20, name: 'Tornado' },
-  ];
+  heroes: Array<Hero> = [];
 
   topHeroes() {
     return this.heroes.slice(-4);
@@ -59,9 +48,17 @@ export class HeroService {
   }
 
   loadHeroes() {
-    const heroes = localStorage.getItem('heroes');
-    if (heroes) {
-      this.heroes = JSON.parse(heroes);
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.http
+        .get<Array<HeroBackend>>('https://code-coaching.dev/api/heroes', {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        })
+        .subscribe((data) => {
+          console.log(data);
+        });
     }
   }
 }
